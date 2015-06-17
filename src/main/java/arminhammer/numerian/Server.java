6fb7ea62b533d1e818f14838ba557972251743fd
Armin Graf <arminhammer@gmail.com>
2015-06-17 15:48:08 -0400

    Added routing layer.

--- src/main/java/arminhammer/numerian/Server.java
+++ src/main/java/arminhammer/numerian/Server.java
@@ -1,6 +1,9 @@
 package arminhammer.numerian;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.BodyHandler;
 
 /**
  * @author Armin Graf
@@ -9,7 +12,21 @@ public class Server extends AbstractVerticle {
 
   @Override
   public void start() {
+
+    Router router = Router.router(vertx);
+
+    router.route().handler(BodyHandler.create());
+    router.get("/").handler(this::handleGetIndex);
+    //router.put("/products/:productID").handler(this::handleAddProduct);
+    //router.get("/products").handler(this::handleListProducts);
+
     // Create an HTTP server which simply returns "Hello World!" to each request.
-    vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World1!")).listen(8080);
+    vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+
   }
+
+  private void handleGetIndex(RoutingContext routingContext) {
+    routingContext.response().end("Hello World!");
+  }
+
 }
